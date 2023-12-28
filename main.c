2fdebf16905af05970e7a59052dd81200a829a31
timotheG <gonin.timothe@gmail.com>
2023-12-28 15:54:50 +0100

    addition

--- main.c
+++ main.c
@@ -3,6 +3,14 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int premierNombre, deuxiemeNombre, resultat = 0;
+    printf("CALCULATOR\n");
+    printf("\nEntrez un premier nombre : ");
+    scanf("%d", &premierNombre);
+    printf("\nEntre un deuxieme nombre : ");
+    scanf("%d", &deuxiemeNombre);
+    resultat = premierNombre + deuxiemeNombre;
+    printf("Le resultat de %d + %d est : %d.", premierNombre, deuxiemeNombre,resultat);
+
     return 0;
 }
